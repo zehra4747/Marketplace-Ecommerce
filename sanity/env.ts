dfc29161942070fb21ec672dd122d538ba46d125
Zehra <163136295+zehra4747@users.noreply.github.com>
2025-02-09 22:58:51 +0500

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,6 +11,11 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const token = assertValue(
+  "skej8VgOBWaHhL3yJtH1PNAoT4Xd9mmcFe0bsmDjqPFCfwDZ1uVc1Q6oionbcNQLDMGc4m2BMpo60aNINMb2JEzsNUFYbvYyQIFoEZViW5QGXvzwiJk4ua48l8G0WRarK5PU1RDeu12uEkayu8JRR8jnlgDokcnc1qAUfJ9EubeJWoQGEXbp",
+  'Missing environment variable: SANITY_API_TOKEN'
+)
+
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
